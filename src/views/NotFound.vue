26df78a45e10dc109bea35c690945bb8e3caa098
Namb2306630 <namhuynh0914923314@gmail.com>
2026-05-25 12:05:50 +0700

    Hien thi danh sach contact lay ve tu server

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang, Trở lại
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<style>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
